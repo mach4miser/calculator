29c626722aab817dccf13687d999cf8c0867b29d
Michael C Riser Jr <michael.c.riser@gmail.com>
2021-06-15 15:44:40 -0700

    Update to files

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const add = document.getElementById('#addition');
+const multiply = document.getElementById('#muliplication');
+const subtract = document.getElementById('#subtraction');
+const divide = document.getElementById('#division');
+const equals = document.getElementById('#equals');
+const con2 = document.querySelector('#container-2');
+
+
+con2.style.color = 'white';
+con2.style.backgroundColor = 'black';
+con2.textContent = 'Hello';
+
